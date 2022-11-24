b94c4477cfd13b8663a20233dd4fe3deacc962f6
twiceand <twiceand@gmail.com>
2022-11-25 06:42:38 +0800

    hello

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/twiceand/twiceand.github.io.git";
+const repoURL = "https://github.com/twiceand/twieand.github.io";
 
 ghpages.publish(
   pathname,
